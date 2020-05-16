6f7159db470ad9e76d3b7dfde8ea46db3da36316
hackroot <lijingze8@outlook.com>
2020-05-16 23:26:35 +0800

    update

--- databatch.py
+++ databatch.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from bert_serving.client import BertClient
 import pickle, os, random
 import define
+import numpy as np
 
 ## reference: https://github.com/uupers/BiliSpider/wiki/%E8%A7%86%E9%A2%91%E5%88%86%E5%8C%BA%E5%AF%B9%E5%BA%94%E8%A1%A8
 relational_table = {
@@ -79,7 +80,7 @@ def expansion(urtext, sz):
     urtext_ = []
     for ut in urtext:
         urtext_.extend([ut for i in range(times)])
-    if len(urtext_) == sz: return urtext
+    if len(urtext_) == sz: return urtext_
     length = len(urtext_); extra = sz - length; interval = (length + extra - 1) // extra
     ret = []
     for i in range(length):
@@ -167,6 +168,59 @@ def get_batch(batch_size, D = None):
         for i in range(batch_size):
             f, L1 = D[now]; fr.append(D[now])
             _X.append(get_new_vector(f))
+            _Y.append(m[L1])
+            now = (now + 1) % len(D)
+        yield _X, _Y, fr
+
+def expansion_mean(urvec, sz):
+    length = urvec.shape[0]; times = sz // length
+    urvec_ = []
+    for uv in urvec:
+        urvec_.extend([uv for i in range(times)])
+    if len(urvec_) == sz: return urvec_
+    length = len(urvec_); extra = sz - length
+    interval = (length + extra - 1) // extra
+    ret = []
+    for i in range(length):
+        ret.append(urvec_[i])
+        if (i + 1) % interval == 0: ret.append(urvec_[i])
+    while len(ret) < sz: ret.append(ret[-1])
+    assert len(ret) == sz
+    return ret
+
+def lessen_mean(urvec, sz):
+    length = urvec.shape[0]; interval = length // sz
+    ret = []; tmp = []
+    a = interval * sz + sz - length; b = sz - a
+    now = 0
+    for i in range(length):
+        now += 1
+        if a > 0:
+            if now == interval: 
+                now = 0; a -= 1 
+                ret.append(np.array(tmp).mean())
+            else: tmp.append(urvec[i])
+        else:
+            if now == interval + 1: 
+                now = 0; b -= 1 
+                ret.append(np.array(tmp).mean())
+            else: tmp.append(urvec[i])
+    assert len(ret) == sz
+    return ret
+
+def zoom_mean(urvec, sz):
+    if urvec.shape[0] == sz: return urvec
+    if urvec.shape[0] < sz: return expansion_mean(urvec, sz)
+    else: return lessen_mean(urvec, sz)
+
+def get_mean_batch(batch_size, D = None):
+    now = 0
+    while True:
+        if now == 0: random.shuffle(D)
+        _X = []; _Y = []; fr = []
+        for i in range(batch_size):
+            f, L1 = D[now]; fr.append(D[now])
+            _X.append(zoom_mean(get_raw_vector(f), sz))
             _Y.append(m[L1])
             now = (now + 1) % len(D)
         yield _X, _Y, fr
