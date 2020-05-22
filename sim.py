d4ddfb41d3d7d2fe5050d60d84cdce5706097d90
hackroot <lijingze8@outlook.com>
2020-05-22 21:06:29 +0800

    update

--- sim.py
+++ sim.py
@@ -7,12 +7,17 @@ def dist(A, B):
     dist = cp.linalg.norm(A - B)
     return 1.0 / (1.0 + dist)
 
+def mdist(A, B):
+    ret = cp.var(A - B) / (cp.var(A) + cp.var(B))
+    return 0.5 * ret
+
 def cossim(A, B):
     num = float(cp.sum(A * B))
     denom = cp.linalg.norm(A) * cp.linalg.norm(B)
     cos = num / denom
-    return 0.5 + 0.5 * cos
-
+    #return 0.5 + 0.5 * cos
+    return cos
+    
 if __name__ == "__main__":
     with open("/home/data/ljz/data/comment_new/vedio_vector_svm", "rb") as f:
         vedio_vector = pickle.load(f)
@@ -23,10 +28,11 @@ if __name__ == "__main__":
     for f, L1 in D:
         A = vedio_vector[f]
         A = cp.array(A)
-        B = databatch.get_raw_vector(f)
-        B = np.array(databatch.zoom_mean(B, databatch.sz)).mean(0)
+        #B = databatch.get_raw_vector(f)
+        #B = np.array(databatch.zoom_mean(B, databatch.sz)).mean(0)
+        
         B = cp.array(B)
-        ret_d.append(cp.asnumpy(dist(A, B)))
+        ret_d.append(cp.asnumpy(mdist(A, B)))
         ret_c.append(cp.asnumpy(cossim(A, B)))
         if len(ret_d) % 10 == 0: print(len(ret_d))
 
