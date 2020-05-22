cde277c9f30f85be558fecd271bea3234a94030c
hackroot <lijingze8@outlook.com>
2020-05-23 02:00:28 +0800

    update

--- model_new.py
+++ model_new.py
@@ -81,25 +81,27 @@ if __name__ == "__main__":
     conv1_x_2 = res_block(conv1_x_1, 4, 4, "conv1_x_2")
 
     #48 * 48
-    conv2_x_1 = res_block(conv1_x_2, 4, 8, "conv2_x_1", downsize = True, attention = CFG.attention)
-    conv2_x_2 = res_block(conv2_x_1, 8, 8, "conv2_x_2")
+    conv2_x_1 = res_block(conv1_x_2, 4, 16, "conv2_x_1", downsize = True, attention = CFG.attention)
+    conv2_x_2 = res_block(conv2_x_1, 16, 16, "conv2_x_2")
 
     #12 * 12
-    conv3_x_1 = res_block(conv2_x_2, 8, 16, "conv3_x_1", downsize = True, attention = CFG.attention)
-    conv3_x_2 = res_block(conv3_x_1, 16, 16, "conv3_x_2")
+    conv3_x_1 = res_block(conv2_x_2, 16, 64, "conv3_x_1", downsize = True, attention = CFG.attention)
+    conv3_x_2 = res_block(conv3_x_1, 64, 64, "conv3_x_2")
     conv3_x_p = tf.nn.avg_pool(value = conv3_x_2,
         ksize = [1, 12, 1, 1],
         strides = [1, 12, 1, 1],
         padding = "SAME"
     )
 
-    ret = tf.reshape(conv3_x_p, [-1, 768, 16])
-    W_f1 = init_w([16, 1], name = "W_f1")
+    ret = tf.reshape(conv3_x_p, [-1, 768, 64])
+    W_f1 = init_w([64, 1], name = "W_f1")
     b_f1 = init_b([768], name = "b_f1")
     f1 = tf.add(tf.reshape(reshape_matmul(ret, W_f1), [-1, 768]), b_f1)
+    f1 = tf.nn.relu6(tf.layers.batch_normalization(f1))
     W_f2 = init_w([768, 96], name = "W_f2")
     b_f2 = init_b([96], name = "b_f2")
     f2 = tf.add(tf.matmul(f1, W_f2), b_f2)
+    f2 = tf.nn.relu6(tf.layers.batch_normalization(f2))
     W_f3 = init_w([96, 12], name = "W_f3")
     b_f3 = init_b([12], name = "b_f3")
     out_ = tf.add(tf.matmul(f2, W_f3), b_f3)   
@@ -152,9 +154,9 @@ if __name__ == "__main__":
         )
         print("step %d, loss %.4f, acc %.4f" % (step, l, a))
         ret_loss.append(l); min_loss = min(l, min_loss)
-        if min(ret_loss[-min(len(ret_loss), 300):]) > min_loss: break
+        if min(ret_loss[-min(len(ret_loss), 400):]) > min_loss: break
         
-        if min(ret_loss[-min(len(ret_loss), 150):]) > min_loss:
+        if min(ret_loss[-min(len(ret_loss), 200):]) > min_loss:
             S_test = databatch.get_mean_batch(batch_size = CFG.batch_size, D = test_D)
             predict_a, predict_l = 0, 0
             for i in range(len(test_D) // CFG.batch_size):
