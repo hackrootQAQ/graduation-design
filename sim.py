0608a055f2fc4dd82148344d23ebff865fd3138f
hackroot <lijingze8@outlook.com>
2020-05-22 17:25:40 +0800

    update

--- sim.py
+++ sim.py
@@ -20,27 +20,14 @@ if __name__ == "__main__":
     with open("./train", "rb") as f: D = pickle.load(f)
     with open("./test", "rb") as f: D.extend(pickle.load(f))
     ret_d = []; ret_c = []
-    """
     for f, L1 in D:
-        try:
-            A = vedio_vector[f]
-            B = databatch.get_raw_vector(f)
-            B = np.array(B).mean(0)
-            ret_d.append(dist(A, B))
-            ret_c.append(cossim(A, B))
-        except:
-            pass
-    """
-    f = "82008182"
-    A = vedio_vector[f]
-    print(A)
-    A = cp.array(A)
-    B = databatch.get_new_vector(f)
-    B = np.array(B).mean(0)
-    print(B)
-    B = cp.array(B)
-    ret_d.append(cp.asnumpy(dist(A, B)))
-    ret_c.append(cp.asnumpy(cossim(A, B)))
+        A = vedio_vector[f]
+        A = cp.array(A)
+        B = databatch.get_new_vector(f)
+        B = np.array(B).mean(0)
+        B = cp.array(B)
+        ret_d.append(cp.asnumpy(dist(A, B)))
+        ret_c.append(cp.asnumpy(cossim(A, B)))
 
     print(min(ret_d), max(ret_d), np.array(ret_d).mean())
     print(min(ret_c), max(ret_c), np.array(ret_c).mean())
